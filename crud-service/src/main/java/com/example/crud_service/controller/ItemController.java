9c6a73d61934d5f8ff6707928a9dc4c498d6db18
Ramses Ramos <ramses1030@live.com.mx>
2025-12-14 19:44:16 -0600

    cambios

--- crud-service/src/main/java/com/example/crud_service/controller/ItemController.java
+++ crud-service/src/main/java/com/example/crud_service/controller/ItemController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/items")
 public class ItemController {
